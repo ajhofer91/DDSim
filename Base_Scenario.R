ac5d79fa6e20b88c57271849c8c234a80238b57a
ajhofer91 <ajhofer91@gmail.com>
2014-08-28 16:07:33 +1000

    Clean up

--- Base_Scenario.R
+++ Base_Scenario.R
@@ -3,38 +3,36 @@
 #----------------------------------------------------------------------------------------------------------------------------------#
 library(ss3sim)
 library(r4ss)
-d<-'C:/Users/Alex/Desktop/Base Scenario'
+d<-'C:/Users/Alex/Desktop/Github/DDSIM/'
+d_base<-paste0(d,'Base_Scenario')
 setwd(d)
 #---------------------------------------------------------------------------------------------------------------------------------#
 
 # Source DDSim functions:
-source('C:/Users/Alex/Desktop/Github/DDSim/R/get-cohort-devs.R') # Alex's Box
-source('C:/Users/Alex/Desktop/Github/DDSim/R/write-growth-casefile.R') 
-source('C:/Users/Alex/Desktop/Github/DDSim/R/run-ddsim.R') 
+source(paste0(d,'R/get-cohort-devs.R')) 
+source(paste0(d,'R/write-growth-casefile.R')) 
+source(paste0(d,'R/run-ddsim.R')) 
 
-source('C:/Dropbox/Github/DDSim/R/get-cohort-devs.R') # Athol's Box
-source('C:/Dropbox/Github/DDSim/R/write-growth-casefile.R') 
-source('C:/Dropbox/Github/DDSim/R/run-ddsim.R') 
 
 #----------------------------------------------------------------------------------------------------------------------------------#
 run_ddsim( d,                        #wd
-           paste0(d,"/Cases"),       #ss3sim input (cases)
-           paste0(d,"/BDD_OM"),      #ss3sim input (om) 
-           paste0(d,"/BDD_EM"),      #ss3sim input (em)
+           paste0(d_base,"/Cases"),       #ss3sim input (cases)
+           paste0(d_base,"/BDD_OM"),      #ss3sim input (om) 
+           paste0(d_base,"/BDD_EM"),      #ss3sim input (em)
            "-spp",
            31,
            0,
            1)
 #----------------------------------------------------------------------------------------------------------------------------------#
-om<-paste0(d,"/D1-E0-F0-R0-G31-spp/1/om")
-em<-paste0(d,"/D1-E0-F0-R0-G31-spp/1/em")
+om<-paste0(d_base,"/D1-E0-F0-R0-G31-spp/1/om")
+em<-paste0(d_base,"/D1-E0-F0-R0-G31-spp/1/em")
 
-SSplotBiology(SS_output(om,covar=F),print=T,plotdir=paste0(d,"/plots/Om")
-SSplotBiology(SS_output(em,covar=F),print=T,plotdir=paste0(d,"/plots/Em")
+SSplotBiology(SS_output(om,covar=F),print=T,plotdir=paste0(d_base,"/plots/Om"))
+SSplotBiology(SS_output(em,covar=F),print=T,plotdir=paste0(d_base,"/plots/Em"))
 
-SSplotNumbers(SS_output(om,covar=F),print=T,plotdir=paste0(d,"/plots/Om")
-SSplotNumbers(SS_output(om,covar=F),print=T,plotdir=paste0(d,"/plots/Em")
+SSplotNumbers(SS_output(om,covar=F),print=T,plotdir=paste0(d_base,"/plots/Om"))
+SSplotNumbers(SS_output(om,covar=F),print=T,plotdir=paste0(d_base,"/plots/Em"))
               
 biglist<-SSgetoutput(dirvec=c(om, em), getcovar=T, forecast=T)              
 biglist<-SSsummarize(biglist)              
-SSplotComparisons(biglist,print=T,plotdir=paste0(d,"/plots"))              
+SSplotComparisons(biglist,print=T,plotdir=paste0(d_base,"/plots"))              
